612bd4af8604ce07ef95fc6846bdf6f07a4fce63
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2024-12-09 09:23:08 +0100

    2024 day05 started

--- 2024/05_PrintQueue/index.js
+++ 2024/05_PrintQueue/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const rawData = fs.readFileSync('testinput.txt', {encoding: 'utf8', flag: 'r'});
-// const rawData = fs.readFileSync('input.txt', {encoding: 'utf8', flag: 'r'});
+// const rawData = fs.readFileSync('testinput.txt', {encoding: 'utf8', flag: 'r'});
+const rawData = fs.readFileSync('input.txt', {encoding: 'utf8', flag: 'r'});
 const rawDataSplit = rawData.replace(/\r/g, "").split('\n\n');
 
 const orderingRows = rawDataSplit[0].split('\n');
@@ -13,14 +13,24 @@ const allNumbersInOrderingRows = orderingRows.join('|').split('|').map(val => Nu
 const allNumbersInOrderingRowsFiltered = allNumbersInOrderingRows.filter((val, index) => allNumbersInOrderingRows.indexOf(val) === index);
 
 let globalPageOrder = [];
+let correctPageMidleNumberList = [];
 
 allNumbersInOrderingRowsFiltered.forEach((pageToOrder) => {
-    console.log('checking ', pageToOrder);
+
+    if (pageToOrder === 33) {
+        console.log('checking: ', pageToOrder);
+        console.log('when global order is: ', globalPageOrder);
+    }
 
     let minIndexToInsertBefore = null;
     let maxIndexToInsertAfter = null;
 
     orderingRows.forEach((orderConstraint) => {
+
+        if (pageToOrder === 33 && orderConstraint === '33|91') {
+            console.log('by constraint: ', orderConstraint);
+        }
+
         const [lesserPage, higherPage] = orderConstraint.split('|').map(val => Number(val));
 
         if (lesserPage === pageToOrder) {
@@ -47,7 +57,11 @@ allNumbersInOrderingRowsFiltered.forEach((pageToOrder) => {
         }
     });
 
-    console.log('current page order: ', globalPageOrder);
+    if (pageToOrder === 33) {
+        console.log('minIndexToInsertBefore: ', minIndexToInsertBefore);
+        console.log('maxIndexToInsertAfter: ', maxIndexToInsertAfter);
+    }
+
     let indexToInsertInto = 0;
 
     if (minIndexToInsertBefore === null && maxIndexToInsertAfter !== null) {
@@ -58,18 +72,41 @@ allNumbersInOrderingRowsFiltered.forEach((pageToOrder) => {
         indexToInsertInto = Math.ceil((maxIndexToInsertAfter + minIndexToInsertBefore) / 2);
     }
 
-    console.log(`${pageToOrder} should be inserted into index ${indexToInsertInto}`);
+    if (pageToOrder === 33) {
+        console.log('inserting ', pageToOrder, ' to index ', indexToInsertInto);
+    }
+
     globalPageOrder.splice(indexToInsertInto, 0, pageToOrder);
-
-
 });
 
-console.log(orderingRows);
+const isCorrectUpdate = (pageRow, correctOrder) => {
+    for (index in pageRow) {
+        const indexNum = Number(index);
+        if (pageRow[indexNum] !== correctOrder[indexNum]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+pageUpdateRows.forEach((pageUpdateRow) => {
+    const pageUpdateRowArr = pageUpdateRow.split(',').map(val => Number(val));
+    const correctOrder = globalPageOrder.filter(page => pageUpdateRowArr.indexOf(page) !== -1);
+    const correctUpdate = isCorrectUpdate(pageUpdateRowArr, correctOrder);
+
+    if (correctUpdate) {
+        correctPageMidleNumberList.push(pageUpdateRowArr[Math.floor(pageUpdateRowArr.length / 2)]);
+    }
+});
+
+const answer_one = correctPageMidleNumberList.reduce((acc, curr) => acc + curr);
+
 // ----- part two -----
 
 
 
 // ----- answers -----
 
-// console.log(answer_one);
+console.log(answer_one);
 // console.log(answer_two);
